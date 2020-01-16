6b3f40cafaf4eb7c8f2b789f45cb0be93ac5bf05
Vasil Velichkov <vasko_v@abv.bg>
2020-01-16 09:24:53 +0200

    HTTP protocol

---
+++ HttpRequester/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequester
+{
+    class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            const string NewLine = "\r\n";
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 1234);
+            tcpListener.Start();
+            while (true)
+            {
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                await using NetworkStream networkStream = tcpClient.GetStream();
+                byte[] requestBytes = new byte[1000000];
+                int bytesRead = networkStream.Read(requestBytes, 0, requestBytes.Length);
+                string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+                string responseText = "<h1>Hello Header</h1>";
+                string response = "HTTP/1.0 200 OK" + NewLine +
+                                  "Server: SoftUniServer/1.0" + NewLine +
+                                  "Content-Type: text/html" + NewLine +
+                                  "Content-Length: 21" + NewLine +
+                                  NewLine +
+                                  responseText;
+
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                networkStream.Write(responseBytes, 0, responseBytes.Length);
+                Console.WriteLine(request);
+                Console.WriteLine(new string('=', 60));
+            }
+        }
+
+        private static async Task HttpRequest()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://softuni.bg");
+            string result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+        }
+    }
+}
